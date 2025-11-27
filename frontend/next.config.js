2b7a6bf762aeda254318c4f41fc1ed46e95909dc
Seyed Yahya Shirazi <shirazi@ieee.org>
2025-11-27 11:18:53 -0800

    Remove basePath for Cloudflare Pages deployment
    App now serves at root. The website middleware will handle
    proxying /image-annotation/* requests.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const basePath = '/image-annotation'
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost'],
     unoptimized: true,
   },
-  basePath: basePath,
-  assetPrefix: basePath,
   output: 'export',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath
-  },
 }
 
 module.exports = nextConfig
